07e1deb781659c080813e54ca8fd07325f89312f
luke <luke.emery-fertitta@locusenergy.com>
2015-12-29 19:36:05 -0500

    fix vertica error message handling

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='sqlalchemy-vertica-python',
-    version='0.1.0',
+    version='0.1.1',
     description='Vertica dialect for sqlalchemy using vertica_python',
     long_description=open("README.rst").read(),
     license="MIT",
-    url='https://github.com/LocusEnergy/https://github.com/LocusEnergy/sqlalchemy-vertica-python',
-    download_url = 'https://github.com/LocusEnergy/sqlalchemy-vertica-python/tarball/0.1.0',
+    url='https://github.com/LocusEnergy/sqlalchemy-vertica-python',
+    download_url = 'https://github.com/LocusEnergy/sqlalchemy-vertica-python/tarball/0.1.1',
     author='Locus Energy',
     author_email='dbio@locusenergy.com',
     packages=[
