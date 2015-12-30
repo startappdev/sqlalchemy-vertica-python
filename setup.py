d1bf957c6710a0d7c15d5051acfb3d6760c8b658
luke <luke.emery-fertitta@locusenergy.com>
2015-12-29 19:47:27 -0500

    version increment for pypi's sake

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='sqlalchemy-vertica-python',
-    version='0.1.1',
+    version='0.1.2',
     description='Vertica dialect for sqlalchemy using vertica_python',
     long_description=open("README.rst").read(),
     license="MIT",
     url='https://github.com/LocusEnergy/sqlalchemy-vertica-python',
-    download_url = 'https://github.com/LocusEnergy/sqlalchemy-vertica-python/tarball/0.1.1',
+    download_url = 'https://github.com/LocusEnergy/sqlalchemy-vertica-python/tarball/0.1.2',
     author='Locus Energy',
     author_email='dbio@locusenergy.com',
     packages=[
