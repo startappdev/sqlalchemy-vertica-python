80b7cae58284055a316327aea34c762e8efcbe4b
luke <luke.emery-fertitta@locusenergy.com>
2016-10-04 09:59:39 -0700

    increment version number to release patch from PR #1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sqlalchemy-vertica-python',
-    version='0.1.2',
+    version='0.1.3',
     description='Vertica dialect for sqlalchemy using vertica_python',
     long_description=open("README.rst").read(),
     license="MIT",
